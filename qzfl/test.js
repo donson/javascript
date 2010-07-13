fb4c265527320142fe5d55ba04354168e1665716
Donson <donson86@gmail.com>
2010-07-13 17:45:08 +0800

    .

---
+++ qzfl/test.js
@@ -0,0 +1,23 @@
+//----object.getType
+//var a = {'a':4};
+//a = 55555;
+//a = 'dsds';
+//a = ['ss','dd'];
+//a = new User();
+//console.debug(QZFL.object.getType(a));
+//console.debug(Object.prototype.toString.call(a));
+
+//----object.map
+//var ma = {
+    //'a' : 'a',
+    //'b' : 'b'
+//};
+//var mb = {
+    //'a' : 'aaa',
+    //'b' : 'bbb'
+//};
+//QZFL.object.map(ma,mb,'a');
+
+//----css
+
+QZFL.css.convertHexColor('#ff0');
